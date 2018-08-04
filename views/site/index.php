163b38d8ec6bf138b7a105f07c30c348e64b41b2
Cadomazurek <ricardo-mazurek@hotmail.com>
2018-08-04 20:28:21 -0300

    Layout Inicial
    Montando a tela inicial do site de locadora de CD/DVD.

---
+++ views/site/index.php
@@ -0,0 +1,50 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Paris Filmes</h1>
+
+        <p class="lead">Todos os grandes CDs e DVS você encontra aqui na Paris Filmes.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>CINEMA</h2>
+
+                <p> O cinema brasileiro tem sido uma das grandes apostas da Paris Filmes nos últimos anos. O investimento foi um novo desafio que deu certo, desde sua primeira aposta, com o longa-metragem De Pernas Pro Ar, culminando com dois dos maiores sucessos de público do cinema nacional, Até Que a Sorte nos Separe e sua sequência, que foram os maiores êxitos do cinema nacional em bilheteria de 2012 e 2013. Filmes de diretores brasileiros com produção internacional também entram nessa lista, como 360, de Fernando Meirelles (com Anthony Hopkins, Jude Law e Rachel Weisz) e 12 Horas, de Heitor Dhalia (com Amanda Seyfried).</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>LANÇAMENTOS</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>INTERNACIONAL</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
